81bf3428b7e49caac312c7fbb32ed8b401fd76ea
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2014-03-29 18:38:07 +0100

    repair authentication and disconnect

--- js/controllers.js
+++ js/controllers.js
@@ -4,21 +4,26 @@ myApp.factory('Message', ['$resource', function ($resource) {
     return $resource('http://localhost:8080/api/timeline');
 }]);
 
-myApp.controller('TimeLineController', function ($scope, $timeout, Message, $cookies, $http) {
+myApp.controller('TimeLineController', function ($scope, $timeout, Message, $cookies, $location) {
     $scope.currentTimeline = 0;
 
     $scope.update = function () {
-        $http.defaults.headers.common['Authorization'] = "Basic " + $cookies.token;
         $scope.messages = Message.query();
         $scope.currentTimeline = $scope.currentTimeline + 1;
-        //$timeout($scope.update, 1000);
+        $timeout($scope.update, 10000);
     };
+
     $scope.update();
+
+    $scope.disconnect = function () {
+        $cookies.token = null;
+        $location.path("/auth");
+    };
 });
 
 myApp.controller('AuthController', function ($http, $scope, $cookies, $base64, $location) {
     $scope.auth = function (email, password) {
-        $cookies.token = $base64.encode(email + " " + password);
+        $cookies.token = $base64.encode(email + ":" + password);
         $http.defaults.headers.common['Authorization'] = "Basic " + $cookies.token;
         $location.path("/timeline");
     };
