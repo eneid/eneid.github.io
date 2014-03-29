123ab501719cd40a7f7ec55f851b0a73032467b1
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2014-03-29 12:04:27 +0100

    Added basic messages loading from local json file

--- js/controllers.js
+++ js/controllers.js
@@ -3,8 +3,10 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', [function() {
-    
+  .controller('MyCtrl1', ['$scope', '$http', function($scope, $http) {
+    $http.get('timeline.json').success(function(data) {
+      $scope.messages = data;
+    });
   }])
   .controller('MyCtrl2', [function() {
 
