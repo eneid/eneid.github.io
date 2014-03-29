06474433377ce13e57267f8726959551377cc0ac
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2014-03-29 17:48:51 +0100

    authentication and bind to backend

--- js/app.js
+++ js/app.js
@@ -2,8 +2,9 @@
 
 
 // Declare app level module which depends on filters, and services
-var myApp = angular.module('myApp', ['ngRoute', 'ui.gravatar', 'ngResource']).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/timeline', {templateUrl: 'partials/timeline.html', controller: 'TimeLineController'});
-  $routeProvider.otherwise({redirectTo: '/timeline'});
-}]);
+var myApp = angular.module('myApp', ['ngRoute', 'ui.gravatar', 'ngResource', 'base64', 'ngCookies']).
+    config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/auth', {templateUrl: 'partials/auth.html', controller: 'AuthController'});
+        $routeProvider.when('/timeline', {templateUrl: 'partials/timeline.html', controller: 'TimeLineController'});
+        $routeProvider.otherwise({redirectTo: '/auth'});
+    }]);
--- js/controllers.js
+++ js/controllers.js
@@ -1,18 +1,25 @@
 'use strict';
 
 myApp.factory('Message', ['$resource', function ($resource) {
-    return $resource('/timeline/:currentTimelineId.json');
+    return $resource('http://localhost:8080/api/timeline');
 }]);
 
-myApp.controller('TimeLineController', ['$scope', '$http', '$timeout', 'Message', function ($scope, $http, $timeout, Message) {
+myApp.controller('TimeLineController', function ($scope, $timeout, Message, $cookies, $http) {
     $scope.currentTimeline = 0;
 
     $scope.update = function () {
-        $scope.messages = Message.query({
-            currentTimelineId: $scope.currentTimeline % 2
-        });
+        $http.defaults.headers.common['Authorization'] = "Basic " + $cookies.token;
+        $scope.messages = Message.query();
         $scope.currentTimeline = $scope.currentTimeline + 1;
-        $timeout($scope.update, 1000);
+        //$timeout($scope.update, 1000);
     };
     $scope.update();
-}]);
+});
+
+myApp.controller('AuthController', function ($http, $scope, $cookies, $base64, $location) {
+    $scope.auth = function (email, password) {
+        $cookies.token = $base64.encode(email + " " + password);
+        $http.defaults.headers.common['Authorization'] = "Basic " + $cookies.token;
+        $location.path("/timeline");
+    };
+});
