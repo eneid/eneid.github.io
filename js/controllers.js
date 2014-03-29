732bb9c6a64c92f9ef9e6afa0b32e3b679ad218d
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2014-03-29 13:53:36 +0100

    fetch timeline every minutes

--- js/controllers.js
+++ js/controllers.js
@@ -3,8 +3,16 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('TimeLineController', ['$scope', '$http', function($scope, $http) {
-    $http.get('timeline.json').success(function(data) {
-      $scope.messages = data;
-    });
-  }]);
+    .controller('TimeLineController', ['$scope', '$http', '$timeout', function ($scope, $http, $timeout) {
+        $scope.currentTimeline = 0;
+
+        $scope.update = function() {
+            $http.get('timeline' + $scope.currentTimeline % 2 + '.json').success(function (data) {
+                $scope.messages = data;
+                $scope.currentTimeline = $scope.currentTimeline + 1;
+                $timeout($scope.update, 60000);
+            });
+        };
+
+        $scope.update();
+    }]);
