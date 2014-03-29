42930be121964fcb21ed911f6333705a63ca3677
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2014-03-29 13:13:12 +0100

    cleanup

--- js/controllers.js
+++ js/controllers.js
@@ -3,11 +3,8 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', '$http', function($scope, $http) {
+  .controller('TimeLineController', ['$scope', '$http', function($scope, $http) {
     $http.get('timeline.json').success(function(data) {
       $scope.messages = data;
     });
-  }])
-  .controller('MyCtrl2', [function() {
-
   }]);
